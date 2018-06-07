59765ad9f0e61000cfc280512397e3c649ee3891
--list <likang08@meituan.com>
2018-06-07 15:09:03 +0800

    2018-06-07

--- script/script.sh
+++ script/script.sh
@@ -1,15 +1,64 @@
 #!/usr/bin/env bash 
-CONST_HOME="$1"
 
-cd $CONST_HOME
-tDirs=$(ls)
-for dir in ${tDirs[*]}; do
-	echo $dir
-done
+foo1() {
+	arr="$@"
+	echo "arr: "$arr
+	echo "arr size: "${#arr[*]}
+	echo "arr size: "${#arr}
+	for k in $arr
+	do
+		echo $k
+	done
+}
 
+foo2() {
+	for k in "$@"
+	do
+		echo $k
+	done
+}
 
+bar1() {
+	arr="$*"
+	echo "arr: "$arr
+	echo "arr size: "${#arr[*]}
+	echo "arr size: "${#arr}
+	for k in $arr
+	do
+		echo $k
+	done
+}
 
+bar2() {
+	for k in "$*"
+	do
+		echo $k
+	done
+}
 
+# foo1 1 2 3 4
+# foo2 a b c d
+# bar1 5 6 7 8
+# bar2 e f g h
+
+baz() {
+	arr1="$@"
+	arr2="$*"
+	if test "$arr1" == "$arr2"; then
+		echo "true"
+	else
+		echo "false"
+	fi
+}
+
+print() {
+	echo $*
+	echo $@
+	echo "$*"
+	echo "$@"
+}
+
+baz 1 2 3 4
 
 
 
